989b2501287706fe6ce0cff81ee9e759077f43ac
Miriam Armero <norma.armero@gmail.com>
2019-05-16 23:33:36 +0200

    Retoque commit anterior (perdido) y añadido de fotos

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+import films from './films.js';
+
+let imgPoster = 'http://image.tmdb.org/t/p/w185/'
+
+window.addEventListener('load', () => {
+    let divFilms = document.querySelector('#films');
+    films.forEach((film) => {
+        let divFilm = document.createElement('article');
+        divFilm.innerHTML = `
+        <img src="${imgPoster}${film.poster_path}" alt="${film.title}>
+        <p class="title">${film.title}</p>
+        <p class="vote">${film.vote_average}</p>
+        <p class="date">${film.release_date}</p>
+        `;
+
+        divFilms.appendChild(divFilm);
+    });
+});
